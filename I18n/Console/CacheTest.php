8b24d70e4ce41f662b12cb6117568c289d6e7f08
Xiangmin Liu <635750556@qq.com>
2018-09-09 11:44:29 +0800

    fix i18n cache command and it tests

--- I18n/Console/CacheTest.php
+++ I18n/Console/CacheTest.php
@@ -20,12 +20,13 @@ declare(strict_types=1);
 
 namespace Tests\I18n\Console;
 
+use Leevel\Bootstrap\Project as Projects;
 use Leevel\Di\IContainer;
 use Leevel\Filesystem\Fso;
 use Leevel\I18n\Console\Cache;
-use Leevel\I18n\II18n;
 use Leevel\Kernel\IProject;
 use Leevel\Option\IOption;
+use Leevel\Option\Option;
 use Tests\Console\BaseCommand;
 use Tests\TestCase;
 
@@ -63,11 +64,25 @@ class CacheTest extends TestCase
 
     public function testBaseUse()
     {
-        $cacheFile = __DIR__.'/i18n_cache.php';
+        $cacheFile = __DIR__.'/i18n_cache_[i18n].php';
 
         $cacheData = [
-            '中国'   => 'china',
-            '成都'   => 'cd',
+            'zh-CN' => [
+            ],
+            'zh-TW' => [
+                '上一页'    => '上一頁',
+                '下一页'    => '下一頁',
+                '共 %d 条' => '共 %d 條',
+                '前往'     => '前往',
+                '页'      => '頁',
+            ],
+            'en-US' => [
+                '上一页'    => 'Previous',
+                '下一页'    => 'Next',
+                '共 %d 条' => 'Total %d',
+                '前往'     => 'Go to',
+                '页'      => 'Page',
+            ],
         ];
 
         $result = $this->runCommand(
@@ -87,23 +102,49 @@ class CacheTest extends TestCase
             $result
         );
 
+        foreach (['zh-CN', 'zh-TW', 'en-US'] as $i18n) {
+            $this->assertContains(
+                $this->normalizeContent(
+                    sprintf(
+                        'I18n file %s cache successed.',
+                        $cacheFileForI18n = str_replace('[i18n]', $i18n, $cacheFile)
+                    )
+                ),
+                $result
+            );
+
+            $this->assertSame($cacheData[$i18n], (array) (include $cacheFileForI18n));
+
+            unlink($cacheFileForI18n);
+        }
+
         $this->assertContains(
-            $this->normalizeContent(sprintf('I18n file %s cache successed.', $cacheFile)),
+            $this->normalizeContent('I18n files cache successed.'),
             $result
         );
-
-        $this->assertSame($cacheData, (array) (include $cacheFile));
-
-        unlink($cacheFile);
     }
 
     public function testDirNotExists()
     {
-        $cacheFile = __DIR__.'/dirNotExists/i18n_cache.php';
+        $cacheFile = __DIR__.'/dirNotExists/i18n_cache_[i18n].php';
 
         $cacheData = [
-            '中国'   => 'china',
-            '成都'   => 'cd',
+            'zh-CN' => [
+            ],
+            'zh-TW' => [
+                '上一页'    => '上一頁',
+                '下一页'    => '下一頁',
+                '共 %d 条' => '共 %d 條',
+                '前往'     => '前往',
+                '页'      => '頁',
+            ],
+            'en-US' => [
+                '上一页'    => 'Previous',
+                '下一页'    => 'Next',
+                '共 %d 条' => 'Total %d',
+                '前往'     => 'Go to',
+                '页'      => 'Page',
+            ],
         ];
 
         $result = $this->runCommand(
@@ -123,25 +164,52 @@ class CacheTest extends TestCase
             $result
         );
 
+        foreach (['zh-CN', 'zh-TW', 'en-US'] as $i18n) {
+            $this->assertContains(
+                $this->normalizeContent(
+                    sprintf(
+                        'I18n file %s cache successed.',
+                        $cacheFileForI18n = str_replace('[i18n]', $i18n, $cacheFile)
+                    )
+                ),
+                $result
+            );
+
+            $this->assertSame($cacheData[$i18n], (array) (include $cacheFileForI18n));
+
+            unlink($cacheFileForI18n);
+        }
+
         $this->assertContains(
-            $this->normalizeContent(sprintf('I18n file %s cache successed.', $cacheFile)),
+            $this->normalizeContent('I18n files cache successed.'),
             $result
         );
 
-        $this->assertSame($cacheData, (array) (include $cacheFile));
-
-        unlink($cacheFile);
         rmdir(dirname($cacheFile));
     }
 
     public function testDirWriteable()
     {
         $dirname = __DIR__.'/dirWriteable';
-        $cacheFile = $dirname.'/i18n_cache.php';
+        $cacheFile = $dirname.'/i18n_cache_[i18n].php';
 
         $cacheData = [
-            '中国'   => 'china',
-            '成都'   => 'cd',
+            'zh-CN' => [
+            ],
+            'zh-TW' => [
+                '上一页'    => '上一頁',
+                '下一页'    => '下一頁',
+                '共 %d 条' => '共 %d 條',
+                '前往'     => '前往',
+                '页'      => '頁',
+            ],
+            'en-US' => [
+                '上一页'    => 'Previous',
+                '下一页'    => 'Next',
+                '共 %d 条' => 'Total %d',
+                '前往'     => 'Go to',
+                '页'      => 'Page',
+            ],
         ];
 
         // 设置目录只读
@@ -182,11 +250,25 @@ class CacheTest extends TestCase
     public function testParentDirWriteable()
     {
         $dirname = __DIR__.'/parentDirWriteable/sub';
-        $cacheFile = $dirname.'/i18n_cache.php';
+        $cacheFile = $dirname.'/i18n_cache_[i18n].php';
 
         $cacheData = [
-            '中国'   => 'china',
-            '成都'   => 'cd',
+            'zh-CN' => [
+            ],
+            'zh-TW' => [
+                '上一页'    => '上一頁',
+                '下一页'    => '下一頁',
+                '共 %d 条' => '共 %d 條',
+                '前往'     => '前往',
+                '页'      => '頁',
+            ],
+            'en-US' => [
+                '上一页'    => 'Previous',
+                '下一页'    => 'Next',
+                '共 %d 条' => 'Total %d',
+                '前往'     => 'Go to',
+                '页'      => 'Page',
+            ],
         ];
 
         // 设置目录只读
@@ -229,33 +311,60 @@ class CacheTest extends TestCase
     protected function initContainerService(IContainer $container, string $cacheFile, array $cacheData)
     {
         // 注册 project
-        $project = $this->createMock(IProject::class);
+        $project = new Project();
         $this->assertInstanceof(IProject::class, $project);
 
-        $project->method('i18nCachedPath')->willReturn($cacheFile);
+        $project->setCacheFile($cacheFile);
 
-        $this->assertEquals($cacheFile, $project->i18nCachedPath('en-US'));
+        foreach (['zh-CN', 'zh-TW', 'en-US'] as $i18n) {
+            $this->assertEquals(
+                str_replace('[i18n]', $i18n, $cacheFile),
+                $project->i18nCachedPath($i18n)
+            );
+        }
 
         $container->singleton(IProject::class, $project);
 
-        // 注册 i18n
-        $i18n = $this->createMock(II18n::class);
-
-        $this->assertInstanceof(II18n::class, $i18n);
-
-        $i18n->method('all')->willReturn($cacheData);
-        $this->assertEquals($cacheData, $i18n->all());
-
-        $container->singleton(II18n::class, $i18n);
-
         // 注册 option
-        $option = $this->createMock(IOption::class);
+        $option = new Option([
+            'app' => [
+                '_composer' => [
+                    'i18ns' => [],
+                ],
+            ],
+            'i18n' => [
+                'default' => 'en-US',
+            ],
+        ]);
 
-        $this->assertInstanceof(IOption::class, $option);
-
-        $option->method('get')->willReturn('en-US');
-        $this->assertEquals('en-US', $option->get());
+        $project->singleton('option', function () use ($option) {
+            return $option;
+        });
 
         $container->singleton(IOption::class, $option);
+    }
+}
+
+class Project extends Projects
+{
+    protected $cacheFile;
+
+    public function setCacheFile(string $cacheFile)
+    {
+        $this->cacheFile = $cacheFile;
+    }
+
+    public function i18nCachedPath(string $i18n): string
+    {
+        return str_replace('[i18n]', $i18n, $this->cacheFile);
+    }
+
+    public function i18nPath(string $path = '')
+    {
+        return __DIR__.'/i18n';
+    }
+
+    protected function registerBaseProvider()
+    {
     }
 }
